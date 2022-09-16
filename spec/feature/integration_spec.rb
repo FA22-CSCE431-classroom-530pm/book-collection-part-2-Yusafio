9dcd005b325af2a3ae9f15eb8406565e1a8632b4
Yusafio <yusuf70704@gmail.com>
2022-09-16 15:54:29 -0500

    lab session 9/16/22

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,22 +1,28 @@
+# location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
-# Example
+def select_date(date, options = {})
+    field = options[:from]
+    base_id = find(:xpath, ".//label[contains(.,'#{field}')]")[:for]
+    year, month, day = date.split(',')
+    select year,  :from => "#{base_id}_1i"
+    select month, :from => "#{base_id}_2i"
+    select day,   :from => "#{base_id}_3i"
+  end
+
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
-    fill_in 'Author', with: 'JK Rowling'
-    fill_in 'Price', with: 19.99
-    fill_in 'Published date', with: '2020-09-28'
-    # if you use date_select in the form for book:
-    # select '2020' :from => 'book_published_date_1i'
-    # select '9' :from => 'book_published_date_2i'
-    # select '28' :from => 'book_published_date_3i'
+    fill_in 'Title', with: 'The Fellowship of the Ring'
+    fill_in 'Author', with: 'J. R. R. Tolkien'
+    fill_in 'Price', with: '9.99'
+    select_date("1954,July,29", :from => "Published")
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
-    expect(page).to have_content('JK Rowling')
-    expect(page).to have_content('19.99')
-    expect(page).to have_content('2020-09-28')
+    expect(page).to have_content('The Fellowship of the Ring')
+    expect(page).to have_content('J. R. R. Tolkien')
+    expect(page).to have_content('9.99')
+    expect(page).to have_content('1954-07-29')
   end
+
 end
